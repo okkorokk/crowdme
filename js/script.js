01222caa2700f4490835b044d63bff6b1c4c1ff0
okkorokk <69345811+okkorokk@users.noreply.github.com>
2020-08-09 16:45:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $(".header-burger").click(function(event){
+$(document).ready(function () {
+  $(".header-burger").click(function (event) {
     $(".header-burger,.header-menu").toggleClass("active");
     $("body").toggleClass("lock");
   });
