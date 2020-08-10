908c6518e33bd0bc7cd930275bcb9435acea20f2
okkorokk <69345811+okkorokk@users.noreply.github.com>
2020-08-10 19:50:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
-  $(".header-burger").click(function (event) {
-    $(".header-burger,.header-menu").toggleClass("active");
-    $("body").toggleClass("lock");
-  });
+	$(".header-burger").click(function (event) {
+		$(".header-burger,.header-menu").toggleClass("active");
+		$("body").toggleClass("lock");
+	});
 });
+
+let tab_items = document.getElementsByClassName("tab-item");
+tab_items[0].style.color = "white";
+for (i = 0; i < tab_items.length; i++) {
+	tab_items[i].onclick = function (event) {
+		let tab = event.target;
+		let tab_id = tab.id;
+		let tab_block = document.getElementsByClassName("tabs-block");
+		for (let i = 0; i < tab_block.length; i++) {
+      tab_block[i].style.display = "none";
+      tab_items[i].style.color = "#656565";
+		}
+		for (let i = 0; i < tab_block.length; i++) {
+			if (tab_block[i].id.includes(tab_id)) {
+        tab_block[i].style.display = "flex";
+        tab.style.color = "white";
+			}
+		}
+	};
+}
