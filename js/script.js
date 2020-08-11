5be48d9f468c0671e8952b01f5d75cd4e5d78fc8
okkorokk <69345811+okkorokk@users.noreply.github.com>
2020-08-11 12:39:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,14 +13,21 @@ for (i = 0; i < tab_items.length; i++) {
 		let tab_id = tab.id;
 		let tab_block = document.getElementsByClassName("tabs-block");
 		for (let i = 0; i < tab_block.length; i++) {
-      tab_block[i].style.display = "none";
-      tab_items[i].style.color = "#656565";
+			tab_block[i].style.display = "none";
+			tab_items[i].style.color = "#656565";
 		}
 		for (let i = 0; i < tab_block.length; i++) {
 			if (tab_block[i].id.includes(tab_id)) {
-        tab_block[i].style.display = "flex";
-        tab.style.color = "white";
+				tab_block[i].style.display = "flex";
+				tab.style.color = "white";
 			}
 		}
 	};
+}
+
+let progressBar = document.getElementsByClassName("progress-bar");
+let funded = document.getElementsByClassName("funded");
+for (let i = 0; i < progressBar.length; i++) {
+	let progressBarValue = progressBar[i].value;
+	funded[i].innerHTML = progressBarValue + "% Funded";
 }
